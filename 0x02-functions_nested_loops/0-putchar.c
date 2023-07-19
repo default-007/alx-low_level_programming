945d848e56844a59af8de644dc00648b1c3b847f
Brian Otieno <brianokola@gmail.com>
2023-07-18 23:46:35 -0700

    Print putchar using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints _putchar using putchar prototype
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchr";
+	int ch;
+	
+	for (ch = 0; ch<8; ++ch)
+		_putchar(str[ch]);
+	_puttchar('\n');
+
+	return (0);
+}
