30b54a067e8a3bd7e90c517fbc72c5f897bbd722
Brian Otieno <brianokola@gmail.com>
2023-07-19 02:26:59 -0700

    Print the alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Print_alphabet_x10 -Function tat will print the alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int l, ch;
+
+	for (l = 0; l <= 9; ++l)
+	{
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
