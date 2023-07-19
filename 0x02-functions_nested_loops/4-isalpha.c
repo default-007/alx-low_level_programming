ee8b00549e0952e7a60e7b3a3c547ef4c9a43e0a
Brian Otieno <brianokola@gmail.com>
2023-07-19 03:22:49 -0700

    Minor bug fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,14 @@
 /**
  * _isalpha - check if character is a letter, both lowercase or uppercase
  *
- * @c :takes imput fron other functions
+ * @c: takes imput fron other functions
  *
- * Return : 1 if c is true else 0
+ * Return: 1 if c is true else 0
  */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c >= 90))
 		return (1);
 	return (0);
 }
