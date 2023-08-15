acd079ef6711d4558a200ea5507f8e356d301309
Brian Otieno <brianokola@gmail.com>
2023-08-15 15:41:04 -0700

    absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+/*
+ * absolute value of a number
+ */
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
+
+#endif /* _FUNCTION_LIKE_MACRO_H */
+
