514f4ea7c1339eed2763740ea60681b61041c6a0
Brian Otieno <brianokola@gmail.com>
2023-09-15 12:29:51 +0000

    add all parameters

---
+++ 0x10-variadic_functions/variadic_function.h
@@ -0,0 +1,14 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * File: main
+ *
+ * Desc: header file
+**/
+int sum_them_all(const unsigned int n, ...);
+
+#endif
